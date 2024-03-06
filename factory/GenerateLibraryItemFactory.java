897719d600f426c6be706422c1510971a713fabf
funmbia <92063639+funmbia@users.noreply.github.com>
2024-03-06 16:15:38 -0500

    Update GenerateLibraryItemFactory.java

--- factory/GenerateLibraryItemFactory.java
+++ factory/GenerateLibraryItemFactory.java
@@ -4,7 +4,7 @@ public class GenerateLibraryItemFactory {
 
 	
 	
-	public LibraryItem getItemType (String type) {
+	public LibraryItem createItemType (String type) {
 		if (type.toLowerCase().equals("newsletter") ) return new Newsletter();
 		else if (type.toLowerCase().equals("onlinebook")) return new OnlineBook();
 		else if (type.toLowerCase().equals("hardcoverbook")) return new HardcoverBook();
@@ -16,7 +16,7 @@ public class GenerateLibraryItemFactory {
 	//QUICK TEST OF FACTORY (done for every item type)
 	public static void main(String[] args) {
 		GenerateLibraryItemFactory fact = new GenerateLibraryItemFactory();
-		CD result = (CD) fact.getItemType("cd");
+		CD result = (CD) fact.createItemType("cd");
 		
 		result.setAttributes("Life of the Johnsons", "1st Florr; Shelf G5",  true, true);
 		
