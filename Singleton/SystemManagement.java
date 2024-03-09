d0d8115e78365297646627e0065666cd15f33a3b
ruivoa <aidanruivo@gmail.com>
2024-03-09 09:56:51 -0500

    Changes

--- Singleton/SystemManagement.java
+++ Singleton/SystemManagement.java
@@ -1,6 +1,39 @@
 package Singleton;
 
-public class SystemManagement {
+import database.UserInfo;
+import factory.LibraryItem;
 
+public class SystemManagement {
+    private static final SystemManagement systemInstance = new SystemManagement();
+
+    public static SystemManagement getSystemInstance(){
+        return systemInstance;
+    }
+
+    private SystemManagement(){}
+
+    public void creatAccount(String type){
+
+    }
+
+    public UserInfo loginUser(String email, String password){
+        return null;
+    }
+
+    public void additem(LibraryItem item){
+
+    }
+
+    public void enableItem(LibraryItem item){
+
+    }
+
+    public void disableItem(LibraryItem item){
+
+    }
+
+    public String providePaymentOptions(){
+        return "";
+    }
 
 }
