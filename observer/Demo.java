94bdda0dfc7915af27629c2f9b1caea20ac26ab6
Yasmine <91900711+yasmine-thandi@users.noreply.github.com>
2024-03-10 00:40:40 -0500

    Create Demo.java

---
+++ observer/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+
+	public static void main(String[] args){
+		LibraryManagementSysInfo subject = new LibraryManagementSysInfo();
+
+		new Faculty(subject);
+		new Student(subject);
+		new NonFaculty(subject);
+		new Visitor(subject);
+		subject.notifyAllObservers();
+	}
+}
