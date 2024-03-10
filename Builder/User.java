123156eb36195848147415420f08474b9cd64dc3
SukyH <162163779+SukyH@users.noreply.github.com>
2024-03-10 00:12:10 -0500

    Update User.java

--- Builder/User.java
+++ Builder/User.java
@@ -1,1 +1,55 @@
+public class User{
+public int itemsOverdue;
+public int penaltyToPay;
+public List<RentalOrder> currentlyRenting;
+public OrderBuilder currentOrder;
+/* why is it here public itemsOut{
+}*/
+ 
+    public User() {
+    }
 
+    
+    public User(int itemsOverdue, int penaltyToPay, List<RentalOrder> currentlyRenting, OrderBuilder currentOrder) {
+        this.itemsOverdue = itemsOverdue;
+        this.penaltyToPay = penaltyToPay;
+        this.currentlyRenting = currentlyRenting;
+        this.currentOrder = currentOrder;
+    }
+
+
+    public int getItemsOverdue() {
+        return itemsOverdue;
+    }
+
+    public void setItemsOverdue(int itemsOverdue) {
+        this.itemsOverdue = itemsOverdue;
+    }
+
+  
+    public int getPenaltyToPay() {
+        return penaltyToPay;
+    }
+
+    public void setPenaltyToPay(int penaltyToPay) {
+        this.penaltyToPay = penaltyToPay;
+    }
+
+  
+    public List<RentalOrder> getCurrentlyRenting() {
+        return currentlyRenting;
+    }
+
+    public void setCurrentlyRenting(List<RentalOrder> currentlyRenting) {
+        this.currentlyRenting = currentlyRenting;
+    }
+
+
+    public OrderBuilder getCurrentOrder() {
+        return currentOrder;
+    }
+
+    public void setCurrentOrder(OrderBuilder currentOrder) {
+        this.currentOrder = currentOrder;
+    }
+}
