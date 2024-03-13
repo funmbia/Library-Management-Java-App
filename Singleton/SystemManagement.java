ab4aded676d1f9735d5c97a36b3b46fc87674ffc
ruivoa <aidanruivo@gmail.com>
2024-03-13 11:42:30 -0400

    Changes

--- Singleton/SystemManagement.java
+++ Singleton/SystemManagement.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 public class SystemManagement extends yorkMembers{
 
     public static ArrayList<User> SystemMembers = new ArrayList<>();
+    public static ArrayList<LibraryItem> itemList = new ArrayList<>();
     private static final SystemManagement systemInstance = new SystemManagement();
 
     public static SystemManagement getSystemInstance(){
@@ -25,15 +26,17 @@ public class SystemManagement extends yorkMembers{
     }
 
     public void additem(LibraryItem item){
-
+        itemList.add(item);
     }
 
     public void enableItem(LibraryItem item){
-
+        if(!itemList.contains(item)){
+            itemList.add(item);
+        }
     }
 
     public void disableItem(LibraryItem item){
-
+        itemList.remove(item);
     }
 
     public String providePaymentOptions(){
