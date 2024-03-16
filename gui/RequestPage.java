ce11503b37e527d7a0a5ca4fe28ca1a56add04a8
Yasmine <91900711+yasmine-thandi@users.noreply.github.com>
2024-03-16 17:57:11 -0400

    Update RequestPage.java

--- gui/RequestPage.java
+++ gui/RequestPage.java
@@ -8,7 +8,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import database.User;
+import observer.User;
 
 public class RequestPage implements Page {
 	User user;
