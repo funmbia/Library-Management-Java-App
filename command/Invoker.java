63e0c30c3767baa955e529adb9d6aa3128c73577
SukyH <162163779+SukyH@users.noreply.github.com>
2024-03-18 18:39:47 -0400

    Update Invoker.java

--- command/Invoker.java
+++ command/Invoker.java
@@ -1,13 +1,35 @@
 package command;
-public class Invoker{
-	private Command command;
-	
-	public void setCommand(Command c) {
-		
-	}
-	
-	public void executeCommand() {
-		
-	}
 
+public class Invoker {
+    private Command command;
+
+    // Constructor
+    public Invoker() {
+        // Initialize command to null or any default value
+        this.command = null;
+    }
+
+    // Constructor with command parameter
+    public Invoker(Command command) {
+        this.command = command;
+    }
+
+    // Getter method for command
+    public Command getCommand() {
+        return command;
+    }
+
+    // Setter method for command
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    // Method to execute the command
+    public void executeCommand() {
+        if (command != null) {
+            command.execute();
+        } else {
+            System.out.println("No command set. Cannot execute.");
+        }
+    }
 }
