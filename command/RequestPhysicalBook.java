9b41a336a54d6d2388b2f20b9aa766f762b09107
SukyH <162163779+SukyH@users.noreply.github.com>
2024-03-18 23:46:05 -0400

    Create RequestPhysicalBook.java

---
+++ command/RequestPhysicalBook.java
@@ -0,0 +1,37 @@
+package command;
+
+public class RequestPhysicalBook implements Command {
+    private String bookToRequest;
+    private String requestType;
+
+    // Constructor
+    public RequestPhysicalBook(String bookToRequest, String requestType) {
+        this.bookToRequest = bookToRequest;
+        this.requestType = requestType;
+    }
+
+    // Getter method for bookToRequest
+    public String getBookToRequest() {
+        return bookToRequest;
+    }
+
+    // Setter method for bookToRequest
+    public void setBookToRequest(String bookToRequest) {
+        this.bookToRequest = bookToRequest;
+    }
+
+    // Getter method for requestType
+    public String getRequestType() {
+        return requestType;
+    }
+
+    // Setter method for requestType
+    public void setRequestType(String requestType) {
+        this.requestType = requestType;
+    }
+
+    @Override
+    public String execute() {
+        // Perform execution logic here
+        return "Request Physical Book - " + bookToRequest + ", Request Type - " + requestType;
+    }
