3fa0d57c4bb36734deadf7dbdab72041721e6907
funmbia <92063639+funmbia@users.noreply.github.com>
2024-03-21 17:04:51 -0400

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        MaintainUser maintainUser = new MaintainUser();
-        User newUser = new User();
-        Command addUser = new AddUserCommand(maintainUser, newUser);
-
-        addUser.execute();
-    }
-}
