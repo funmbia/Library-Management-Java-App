371c2589fee02bcbeff29d75a80060d7413441ec
funmbia <92063639+funmbia@users.noreply.github.com>
2024-03-21 19:48:35 -0400

    Add files via upload

--- Singleton/SystemManagement.java
+++ Singleton/SystemManagement.java
@@ -1,17 +1,25 @@
 package Singleton;
-
+ 
+import observer.MaintainUser;
 import observer.User;
 import factory.LibraryItem;
+import factory.PhysicalItem;
 
 import java.util.ArrayList;
 
 public class SystemManagement extends yorkMembers{
 
-    public static ArrayList<User> SystemMembers = new ArrayList<>();
+	 
+	 private String csvFilePath = "src/csv files/userInfo.csv";
+    public static ArrayList<User> SystemMembers = new ArrayList<>(); //all users
     public static ArrayList<LibraryItem> itemList = new ArrayList<>();
-    private static final SystemManagement systemInstance = new SystemManagement();
+    private static SystemManagement systemInstance;
+   
 
     public static SystemManagement getSystemInstance(){
+    	if (systemInstance == null) {
+    		systemInstance = new SystemManagement();
+    	}
         return systemInstance;
     }
 
@@ -20,25 +28,37 @@ public class SystemManagement extends yorkMembers{
     public void createAccount(User u){
         SystemMembers.add(u);
     }
-
-    public User loginUser(User u){
-        return SystemMembers.get(SystemMembers.indexOf(u));
+ 
+    public User loginUser(String email, String password) throws Exception{
+    	MaintainUser maintain = new MaintainUser(csvFilePath);
+    	maintain.load();
+    	SystemMembers = maintain.getUsers();
+    	
+    	for (User u : SystemMembers) {
+    		if (u.getEmail().equals(email) && u.getPassword().equals(password)) return SystemMembers.get(SystemMembers.indexOf(u));
+    	}
+    	return null;
     }
 
     public void additem(LibraryItem item){
+    	//TODO add to database
         itemList.add(item);
     }
 
-    public void enableItem(LibraryItem item){
+    public void enableItem(PhysicalItem item){
+    	item.setRentable(true);
         if(!itemList.contains(item)){
             itemList.add(item);
         }
+        
     }
 
-    public void disableItem(LibraryItem item){
+    public void disableItem(PhysicalItem item){
+    	item.setRentable(false);
         itemList.remove(item);
     }
 
+    //in purchase command already
     public String providePaymentOptions(){
         return "";
     }
--- Singleton/yorkMembers.java
+++ Singleton/yorkMembers.java
@@ -6,12 +6,13 @@ import observer.User;
 import java.util.ArrayList;
 
 public class yorkMembers {
+	//faculty, student, non-faculty
 
-    public static ArrayList<User> members = new ArrayList<>();
+    public static ArrayList<User> members = new ArrayList<>(); 
 
     //check if registered member already exists
     public boolean validate(User user) {
-    	if(user.isValidEmail(user.getEmail())&&user.isValidPassword(user.getPassword())&& !members.contains(user)) {
+    	if(! members.contains(user)) {
     		return true;
     	}
         return false;
@@ -24,6 +25,10 @@ public class yorkMembers {
 			}
 		}
 		return null;
+	}
+	
+	public static void addUserToYorkMembers(User user) {
+		members.add(user);
 	}
 
 }
