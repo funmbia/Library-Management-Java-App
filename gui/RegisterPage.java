fe6751d868be387fe5364e240d26f7818142f261
funmbia <92063639+funmbia@users.noreply.github.com>
2024-03-21 19:58:37 -0400

    Add files via upload

--- gui/RegisterPage.java
+++ gui/RegisterPage.java
@@ -10,45 +10,56 @@ import observer.User;
 
 //TODO code to lead to action page iff valid user
 public class RegisterPage extends JFrame implements Page { 
+//	
+//	private JTextField nameField;
+//	private JTextField emailField;
+//	private JPasswordField passwordField;
+//	private JTextField accountField;
 	
-	private JTextField nameField;
-	private JTextField emailField;
-	private JPasswordField passwordField;
-	private JTextField accountField;
-	private JButton register;
-	    
-	public JPanel createPage(JFrame frame) {
+	public JPanel createPage(JFrame frame) { 
     	JPanel panel = new JPanel();
         panel.add(new JLabel("Register A New Account"));
           
 
         panel.setLayout(new GridLayout(13, 6));
 
-        JLabel nameLabel = new JLabel("Name:");
+        JLabel nameLabel = new JLabel("Name:"); 
         JLabel emailLabel = new JLabel("Email:");
         JLabel passwordLabel = new JLabel("Password:");
         JLabel accountLabel = new JLabel("Account type:");
         
 
-        nameField = new JTextField(SwingConstants.RIGHT);
-        emailField = new JTextField(SwingConstants.RIGHT);
-        passwordField = new JPasswordField();
-        accountField = new JTextField(SwingConstants.RIGHT);
-       
+        JTextField nameField = new JTextField(50);
+        JTextField emailField = new JTextField(SwingConstants.RIGHT);
+        JPasswordField passwordField = new JPasswordField();
+        JTextField accountField = new JTextField(SwingConstants.RIGHT);
 
         JButton signUp = new JButton("Sign Up");
         signUp.setAlignmentY(JButton.BOTTOM_ALIGNMENT);
 
         signUp.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent e){
             	String name = nameField.getText();
                 String email = emailField.getText();
                 String password = new String(passwordField.getPassword());
                 String account = accountField.getText();
-                registerUser User = new registerUser(name,email,password,account);
-                                          
-                signUp.addActionListener(  t -> new ActionPage(Singleton.yorkMembers.getMember(User.getEmail()),true));             
-            }
+                registerUser user = new registerUser(name,email,password,account);
+                
+                try {
+                    if (!user.createAccount()) {
+                    	JOptionPane.showMessageDialog(null, user.getAccountCreationErrorReason(), "Account Creation Error", JOptionPane.PLAIN_MESSAGE);
+                    } else {
+                        JPanel newpanel = new LogInPage().createPage(frame);
+                        frame.getContentPane().removeAll();
+                        frame.getContentPane().add(newpanel);
+                        frame.revalidate();
+                        frame.repaint();
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }                      
+                             
+            } 
         });
         
         panel.add(nameLabel);
@@ -66,9 +77,9 @@ public class RegisterPage extends JFrame implements Page {
         
         panel.add(signUp);
 
-        add(panel);
-
-        setVisible(true);
+//        add(panel);
+//
+//        setVisible(true);
         return panel; 
 	}
    
