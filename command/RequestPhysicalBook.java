060c365f82b44a8121a73388babcae90f3b1b7b1
funmbia <92063639+funmbia@users.noreply.github.com>
2024-03-22 20:32:42 -0400

    Add files via upload

--- command/RequestPhysicalBook.java
+++ command/RequestPhysicalBook.java
@@ -1,8 +1,7 @@
 package command;
 
 public class RequestPhysicalBook implements Command {
-	public static int[] numberOfRequests = new int[2]; //[0] = high priority requests
-	
+	public static int[] numberOfRequests = new int[2]; //numberOfRequests[0]= high priority
     public String bookToRequest;
     public String requestType;
 
