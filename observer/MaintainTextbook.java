34c344b0a94c299183232870d55de4f01a634bef
Yasmine <91900711+yasmine-thandi@users.noreply.github.com>
2024-04-04 18:05:23 -0400

    Update MaintainTextbook.java

--- observer/MaintainTextbook.java
+++ observer/MaintainTextbook.java
@@ -8,58 +8,56 @@ import com.csvreader.CsvWriter;
 
 public class MaintainTextbook {
 
-    public ArrayList<Textbook> textbooks = new ArrayList<>();
-    public String path;
+	public ArrayList<Textbook> textbooks = new ArrayList<>();
+	public String path;
 
-    public MaintainTextbook(String path) {
-    	this.path = path;
-    }
+	public MaintainTextbook(String path) {
+		this.path = path;
+	}
 
-    public void load() throws Exception {
-        CsvReader reader = new CsvReader(path);
-        reader.readHeaders();
+	public void load() throws Exception {
+		CsvReader reader = new CsvReader(path);
+		reader.readHeaders();
 
-        while (reader.readRecord()) {
-        	Textbook textbooks = new Textbook();
-            String textName = reader.get("textName");
-            String ISBN = reader.get("ISBN");
-            String edition = reader.get("edition");
-            String url = reader.get("url");
-            Textbook textbook = new Textbook(textName, ISBN, edition, url);
-            textbooks.add(textbook);
-        }
+		while (reader.readRecord()) {
+			String textName = reader.get("textName");
+			String ISBN = reader.get("ISBN");
+			String edition = reader.get("edition");
+			String url = reader.get("url");
+			Textbook textbook = new Textbook(textName, ISBN, edition, url);
+			textbooks.add(textbook);
+		}
 
-        reader.close();
-    }
+		reader.close();
+	}
 
-    public void update() throws Exception {
-        CsvWriter csvOutput = new CsvWriter(new FileWriter(path, false), ',');
+	public void update() throws Exception {
+		CsvWriter csvOutput = new CsvWriter(new FileWriter(path, false), ',');
 
-        csvOutput.write("textName");
-        csvOutput.write("ISBN");
-        csvOutput.write("edition");
-        csvOutput.endRecord();
+		csvOutput.write("textName");
+		csvOutput.write("ISBN");
+		csvOutput.write("edition");
+		csvOutput.endRecord();
 
-        for (Textbook textbook : textbooks) {
-            csvOutput.write(textbook.getTextName());
-            csvOutput.write(textbook.getISBN());
-            csvOutput.write(textbook.getEdition());
-            csvOutput.write(textbook.getURL());
-            csvOutput.endRecord();
-        }
+		for (Textbook textbook : textbooks) {
+			csvOutput.write(textbook.getTextName());
+			csvOutput.write(textbook.getISBN());
+			csvOutput.write(textbook.getEdition());
+			csvOutput.write(textbook.getURL());
+			csvOutput.endRecord();
+		}
 
-        csvOutput.close();
-        System.out.println("CSV file has updated.");
-    }
+		csvOutput.close();
+	}
 
-    public String findNewestEd() {
-        String newestEd = "0";
+	public String findNewestEd() {
+		String newestEd = "0";
 
-        for (Textbook textbook : textbooks) {
-            if (textbook.getEdition().compareTo(newestEd)> 0) {
-            	newestEd = textbook.getEdition();
-            }
-        }
-        return newestEd;
-    }
+		for (Textbook textbook : textbooks) {
+			if (textbook.getEdition().compareTo(newestEd)> 0) {
+				newestEd = textbook.getEdition();
+			}
+		}
+		return newestEd;
+	}
 }
