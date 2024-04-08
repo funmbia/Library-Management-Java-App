bee521c0cfa50e46828054b4218630d4ffb2bd5b
funmbia <92063639+funmbia@users.noreply.github.com>
2024-04-08 16:44:02 -0400

    Update Student.java

--- observer/Student.java
+++ observer/Student.java
@@ -143,7 +143,7 @@ public class Student extends User {
 //		System.out.println("Library Manangement System has been notified.");
 //	}
 
-	private long calculateDaysOverdue(Date currentDate, Date dueDate) {
+	public long calculateDaysOverdue(Date currentDate, Date dueDate) {
 		long diffM = Math.abs(currentDate.getTime() - dueDate.getTime());
 		long diffDays = diffM / (1000 * 60 * 60 * 24);
 		return diffDays;
